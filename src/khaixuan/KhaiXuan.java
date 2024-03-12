7fc5c245baaf23a7a69d0da7ef4eb8645271fbca
nc-dat <datcao2404@gmail.com>
2024-03-12 19:46:21 +0700

    demo

---
+++ src/khaixuan/KhaiXuan.java
@@ -0,0 +1,12 @@
+
+package khaixuan;
+
+public class KhaiXuan {
+
+
+    public static void main(String[] args) {
+        System.out.println("Xin chào 2024. Chúc mọi điều như ý");
+        
+    }
+    
+}
